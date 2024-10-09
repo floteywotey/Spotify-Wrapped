4eb35639121f0bf44460576e5d43b08e28b19078
floteywotey <noreply.ethank@gmail.com>
2024-10-09 16:15:18 -0400

    .env config test

--- SpotifyWrapped/SpotifyWrapped/settings.py
+++ SpotifyWrapped/SpotifyWrapped/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = config("APIKEY")
+
+if __name__ == '__main__':
+    print(SECRET_KEY)
 
 
 # Quick-start development settings - unsuitable for production
