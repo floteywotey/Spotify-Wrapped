4aed2b51744a5b1d4c024ba96146f91a82a57938
Eris Wang <eriswang2304@gmail.com>
2024-10-19 00:22:15 -0400

    updated allowed hosts

--- SpotifyWrapped/SpotifyWrapped/settings.py
+++ SpotifyWrapped/SpotifyWrapped/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["3.140.248.253"]
+ALLOWED_HOSTS = ["3.140.248.253","127.0.0.1","localhost"]
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 # Application definition
