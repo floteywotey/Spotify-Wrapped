27ed1bc192f6b45eecfc8e023230211cd051d18d
Kenny Nguyen <127994093+KTNKenny@users.noreply.github.com>
2024-10-19 23:34:17 -0400

    Separate user login from spotify

--- SpotifyWrapped/system/settings.py
+++ SpotifyWrapped/system/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'spotifywrapp', 'templates', 'SpotifyWrapped')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,6 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'spotifywrapp', 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
