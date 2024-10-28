7a8df6babfadefba38161dbfb99c75527abee34a
floteywotey <noreply.ethank@gmail.com>
2024-10-27 22:56:24 -0400

    Update settings.py

--- SpotifyWrapped/system/settings.py
+++ SpotifyWrapped/system/settings.py
@@ -91,8 +91,8 @@ WSGI_APPLICATION = 'system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'spotifywrappeddb',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         'USER': 'admin',
         'PASSWORD': 'password',
         'HOST': 'localhost',
