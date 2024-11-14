fc36c4a1e7287b469e36dea5fadd287af2e09379
floteywotey <noreply.ethank@gmail.com>
2024-11-13 19:21:30 -0500

    update profile page

--- SpotifyWrapped/system/urls.py
+++ SpotifyWrapped/system/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('spotifywrapp.urls')),
+    path('password/', include('django.contrib.auth.urls')),
 ]
 
